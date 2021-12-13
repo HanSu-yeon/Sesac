4a9dec17393a11446f06ac0d59cee0823310bd94
HanSu-yeon <js7122420@gmail.com>
2021-12-13 19:27:56 +0900

    review

--- react/react-project/src/Average.js
+++ react/react-project/src/Average.js
@@ -1,51 +1,60 @@
 import React, { useState, useMemo, useCallback, useRef } from "react";
 
-//전달된 배열의 값의 평균을 반환하는 함수
-const getAverage = (numbers) => {
-  console.log("평균값 계산중");
-  //빈배열일 경우 0을 반환
-  if (numbers.length === 0) return 0;
-
-  //평균 = 총합을 계산해서 배열의 길이로 나눔
-  const sum = numbers.reduce((a, b) => a + b);
-  return sum / numbers.length;
-};
+//useMemo를 사용하면 함수형 컴포넌트 내부에서 발생하는 연산을 최적화할 수 있다
+//1.리스트에 숫자를 추가하면 추가된 숫자들의 평균을 보여 주는 함수형 컴포넌트 작성
 
 const Average = () => {
-  const [list, setList] = useState([]); //숫가값을 담고 있는 리스트
-  const [number, setNumber] = useState(""); //입력창에 입력한 숫자
-  const inputElement = useRef(null);
-
+  //상태변경하려면 useState사용
+  const [num, setNum] = useState(""); //입력창에 입력한 숫자
+  const [numList, setNumList] = useState([]); //숫자값을 담고 있는 리스트
+  const inputTag = useRef(null);
+  //값이 바뀔 때마다 호출
   const handleChange = useCallback((e) => {
-    console.log("핸들체인지");
+    setNum(e.target.value);
+    console.log("처음 렌더링될때만");
+  }, []); //컴포넌트가 처음 렌더링될 때만 함수 생성
 
-    setNumber(e.target.value);
-  }, []); //빈 배열= 마운트될 때만 handleChange함수를 생성
-  const handleClick = useCallback(() => {
-    //list(배열)에 number를 추가 =>업데이트된 새 배열을 반환하는 concat함수를 사용
-    //문자열 number를 parseInt함수를 이용해서 숫자로 변경해서 배열에 추가
-    console.log("핸들클릭");
-    const newList = list.concat(parseInt(number));
-    setList(newList);
-    setNumber("");
-    inputElement.current.focus();
-  }, [number, list]); //number, list가 변경될 때 handleClick함수를 생성
-  //[list]배열의 내용이 바뀔때만 getAverage함수를 실행해라
-  const avg = useMemo(() => getAverage(list), [list]);
+  const handleClick = useCallback(
+    (e) => {
+      //numList배열에 num을 추가 =>업데이트된 새 배열을 반환하는 concat함수를 사용
+      //문자열 number를 parseInt함수를 이용해서 숫자로 변경 후 배열에 추가
+
+      const nextList = numList.concat(parseInt(num));
+      setNumList(nextList);
+      setNum("");
+      inputTag.current.focus();
+      console.log("함수생성");
+    },
+    [num, numList]
+  ); //num 혹은 numList가 바뀌었을 때만 함수 생성
+
+  //전달된 배열 값의 평균을 반환하는 함수
+  const getAverage = (numbers) => {
+    console.log("평균값 계산 중 .....");
+    //빈 배열인 경우 0을 반환
+    if (numbers.length === 0) return 0;
+    // reduce함수 => 배열.reduce((누적값, 현잿값,인덱스,요소)=>{return 결과},초깃값)
+    const sum = numbers.reduce((a, b) => a + b);
+    return sum / numbers.length; //평균
+  };
+
+  //useMemo사용해 최적화하기
+  //numList가 변경되면 getAverage()함수를 실행
+  const avg = useMemo(() => getAverage(numList), [numList]);
   return (
-    <>
+    <div>
+      <input value={num} onChange={handleChange} ref={inputTag} />
+      <button onClick={handleClick}>계산</button>
+      <ul>
+        {numList.map((value, idx) => {
+          return <li key={idx}>{value}</li>;
+        })}
+      </ul>
       <div>
-        <input value={number} onChange={handleChange} ref={inputElement} />
-        <button onClick={handleClick}>등록</button>
-        <div>
-          {list.map((v, i) => {
-            return <span key={i}> {v}+</span>;
-          })}
-        </div>
-        <div>평균값: {avg}</div>
-        <div>입력값: {number}</div>
+        <b>평균:</b> {avg}
       </div>
-    </>
+    </div>
   );
 };
+
 export default Average;
