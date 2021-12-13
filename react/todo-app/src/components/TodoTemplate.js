223d29eca2c9d95a276d238627330c3866e64b0a
HanSu-yeon <js7122420@gmail.com>
2021-12-13 20:35:57 +0900

    복습중(미완)

--- react/todo-app/src/components/TodoTemplate.js
+++ react/todo-app/src/components/TodoTemplate.js
@@ -1,10 +1,13 @@
-import React from "react";
-import "./TodoTemplate.scss";
+//rsc단축명
+import React from 'react';
+import './TodoTemplate.scss';
 
+// - 화면을 가운데 정렬시켜 주며, 앱 타이틀(일정 관리)을 보여 줌
+// - children으로 내부 JSX를 props로 받아 와서 렌더링해 준다
 const TodoTemplate = ({ children }) => {
   return (
     <div className="TodoTemplate">
-      <div className="app-title">일정 관리</div>
+      <div className="app-title">일정관리</div>
       <div className="content">{children}</div>
     </div>
   );
