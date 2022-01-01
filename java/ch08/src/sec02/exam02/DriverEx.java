257bc65f6351e5a9042fcd2caca313256369ee33
Han-su512 <js7122420@gmail.com>
2022-01-02 01:37:52 +0900

    매개변수의 다형성과 강제타입 변환 연습

--- java/ch08/src/sec02/exam02/DriverEx.java
+++ java/ch08/src/sec02/exam02/DriverEx.java
@@ -11,6 +11,17 @@ public class DriverEx {
 
 		driver.drive(bus); // 자동 타입 변환: Vehicle vehicle =bus;
 		driver.drive(taxi);// Vehicle vehicle =taxi;
+
+		// 인터페이스로 구현 객체 사용하기
+		// 인터페이스 타입으로 변수 선언하고 구현 객체를 대입
+		Vehicle vehicle = new Bus();
+		// vehicle.checkFare(); =>Vehicle 인터페이스에는 checkFare()가 없다
+
+		// 강제 타입 변환
+		Bus bus2 = (Bus) vehicle;
+		bus2.run();
+		bus2.checkFare(); // Bus클래스에는 checkFare()가 있음
+
 	}
 
 }
