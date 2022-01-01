6f7b8f72a52fa85ab083dd328dad7682e50cad91
Han-su512 <js7122420@gmail.com>
2022-01-02 01:37:52 +0900

    매개변수의 다형성과 강제타입 변환 연습

--- java/ch08/src/sec02/exam02/Bus.java
+++ java/ch08/src/sec02/exam02/Bus.java
@@ -8,4 +8,7 @@ public class Bus implements Vehicle {
 		System.out.println("버스가 달린다");
 	}
 
+	public void checkFare() {
+		System.out.println("승차요금을 체크한다");
+	}
 }
--- java/ch08/src/sec02/exam02/Driver.java
+++ java/ch08/src/sec02/exam02/Driver.java
@@ -4,5 +4,11 @@ package sec02.exam02;
 public class Driver {
 	public void drive(Vehicle vehicle) {
 		vehicle.run(); // 구현 객체의 run()메소드가 실행됨
+
+		// vehicle 매개 변수가 참조하는 객체가 Bus인지 조사
+		if (vehicle instanceof Bus) {
+			Bus bus = (Bus) vehicle; // Bus객체일 경우 안전하게 강제 타입 변환
+			bus.checkFare(); // Bus 타입으로 강제 타입 변환을 하는 이유 (인터페이스에 이 메소드 없기 때문)
+		}
 	}
 }
